b20ac3e9a929eed93279fb9e21597f8d9f63612f
jleo3 <joseph.leo3@gmail.com>
2015-08-21 17:11:19 -0400

    Create objects for Login and Populate

---
+++ ffl.rb
@@ -0,0 +1,16 @@
+require_relative "lib/sheet_populator"
+require_relative "lib/google_api_wrapper"
+
+require "dotenv"
+require "fantasy_football_nerd"
+
+Dotenv.load
+FFNerd.api_key = ENV["FFNERD_API_KEY"]
+
+google_api_wrapper = FFN::GoogleAPIWrapper.new
+google_api_wrapper.login
+sheets_doc = google_api_wrapper.retrieve_doc("2015 Projections FFN")
+
+sheet_populator = FFN::SheetPopulator.new(sheets_doc)
+sheet_populator.populate
+

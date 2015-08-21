fe2b47bf127f93252db497e73877542897b88949
jleo3 <joseph.leo3@gmail.com>
2015-08-20 22:21:56 -0400

    Write QB Projections to FFN Doc

--- play.rb
+++ play.rb
@@ -1,9 +1,11 @@
+require "pry"
 require "fantasy_football_nerd"
 require "dotenv"
 require "google/api_client"
 require "google_drive"
 
 Dotenv.load
+FFNerd.api_key = ENV["FFNERD_API_KEY"]
 
 client = Google::APIClient.new(application_name: "FF Nerd",
                                application_version: "0.0.1")
@@ -18,16 +20,39 @@ auth.refresh_token = ENV["GOOGLE_REFRESH_TOKEN"]
 auth.refresh!
 
 session = GoogleDrive.login_with_oauth(auth.access_token)
+projections_doc = session.files.detect { |f| f.title == "2015 Projections FFN" }
+worksheet = projections_doc.worksheets[0]
+worksheet.title = "QB"
+worksheet[1,1] = "Player"
+worksheet[1,2] = "Team"
+worksheet[1,3] = "Completions"
+worksheet[1,4] = "Passing Yards"
+worksheet[1,5] = "Passing TDs"
+worksheet[1,6] = "INT"
+worksheet[1,7] = "Rush Yards"
+worksheet[1,8] = "Rush TDs"
+worksheet[1,9] = "Fumbles Lost"
 
-# Gets list of remote files.
-session.files.each do |file|
-  p file.title
+projections = FFNerd.draft_projections("QB")
+projections.count.times do |i|
+  player = projections[i]
+  worksheet[i+2,1] = player.display_name
+  worksheet[i+2,2] = player.team
+  worksheet[i+2,3] = player.completions
+  worksheet[i+2,4] = player.passing_yards
+  worksheet[i+2,5] = player.passing_td
+  worksheet[i+2,6] = player.passing_int
+  worksheet[i+2,7] = player.rush_yards
+  worksheet[i+2,8] = player.rush_td
+  worksheet[i+2,9] = player.fumbles
 end
 
-FFNerd.api_key = ENV["FFNERD_API_KEY"]
+worksheet.save
 
-top_pick = FFNerd.draft_projections("QB").first.displayName
-puts "the number one pick in the draft will be #{top_pick}"
+
+
+#top_pick = FFNerd.draft_projections("QB").first.displayName
+#puts "the number one pick in the draft will be #{top_pick}"
 
 =begin
 puts("1. Open this page:\n%s\n\n" % auth.authorization_uri)

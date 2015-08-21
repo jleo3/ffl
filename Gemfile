29a9dda4160ab62cb6cb6b8cb94671499e55eb58
jleo3 <joseph.leo3@gmail.com>
2015-08-20 21:34:33 -0400

    Use OAuth2 to login to Google's API
    - This is actually not straightforward. The most recent code I used is
      some version of this: http://stackoverflow.com/a/26807195
    
    - TODO: Determine if the token is going to expire using `auth.expires_at`.
      Figure out how to rotate the `auth.access_token` when it expires.
    
    - The Google Drive gem docs: https://github.com/gimite/google-drive-ruby
    
    - This also looks like a promising solution:
      https://github.com/gimite/google-drive-ruby/issues/126#issuecomment-73542381

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
-source 'https://rubygems.org'
+source "https://rubygems.org"
 
-gem 'fantasy_football_nerd', '~> 1.1.3'
-gem 'dotenv', '~> 2.0.2'
+gem "fantasy_football_nerd", "~> 1.1.3"
+gem "dotenv", "~> 2.0.2"
+gem "google_drive", "~> 1.0.1"
+
--- play.rb
+++ play.rb
@@ -1,9 +1,39 @@
-require 'fantasy_football_nerd'
-require 'dotenv'
+require "fantasy_football_nerd"
+require "dotenv"
+require "google/api_client"
+require "google_drive"
+
 Dotenv.load
+
+client = Google::APIClient.new(application_name: "FF Nerd",
+                               application_version: "0.0.1")
+
+auth = client.authorization
+auth.client_id = ENV["GOOGLE_CLIENT_ID"]
+auth.client_secret = ENV["GOOGLE_CLIENT_SECRET"]
+auth.scope = "https://docs.google.com/feeds/" + "https://www.googleapis.com/auth/drive " + "https://spreadsheets.google.com/feeds/"
+auth.redirect_uri = "urn:ietf:wg:oauth:2.0:oob"
+auth.refresh_token = ENV["GOOGLE_REFRESH_TOKEN"]
+
+auth.refresh!
+
+session = GoogleDrive.login_with_oauth(auth.access_token)
+
+# Gets list of remote files.
+session.files.each do |file|
+  p file.title
+end
 
 FFNerd.api_key = ENV["FFNERD_API_KEY"]
 
-top_pick = FFNerd.draft_projections('QB').first.displayName
+top_pick = FFNerd.draft_projections("QB").first.displayName
 puts "the number one pick in the draft will be #{top_pick}"
+
+=begin
+puts("1. Open this page:\n%s\n\n" % auth.authorization_uri)
+puts("2. Enter the authorization code shown in the page: ")
+
+puts "access token: #{auth.access_token}"
+puts "refresh token: #{auth.refresh_token}"
+=end
 
